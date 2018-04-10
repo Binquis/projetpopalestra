bbdbaa75fe2a1bdec593681fd4231a90c145bd10
bianca <biancatdh@gmail.co>
2018-04-09 21:47:07 -0300

    Projeto palestra - deploy

---
+++ siteAdmin/index.php
@@ -0,0 +1,205 @@
+<?php 
+
+	session_start();
+	if(!isset($_SESSION['login'])){
+		header("Location: /cursos-online/siteadmin/login.html");
+	}
+
+?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Bootstrap Admin Theme v3</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Bootstrap -->
+    <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <!-- styles -->
+    <link href="css/styles.css" rel="stylesheet">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+  	<div class="header">
+	     <div class="container">
+	        <div class="row">
+	           <div class="col-md-5">
+	              <!-- Logo -->
+	              <div class="logo">
+	                 <h1><a href="../">Admin CSI</a></h1>
+	              </div>
+	           </div>
+	           <div class="col-md-5">
+	              <div class="row">
+	                <div class="col-lg-12">
+	                  <div class="input-group form">
+	                       <input type="text" class="form-control" placeholder="Search...">
+	                       <span class="input-group-btn">
+	                         <button class="btn btn-primary" type="button">Search</button>
+	                       </span>
+	                  </div>
+	                </div>
+	              </div>
+	           </div>
+	           <div class="col-md-2">
+	              <div class="navbar navbar-inverse" role="banner">
+	                  <nav class="collapse navbar-collapse bs-navbar-collapse navbar-right" role="navigation">
+	                    <ul class="nav navbar-nav">
+	                      <li class="dropdown">
+	                        <a href="#" class="dropdown-toggle" data-toggle="dropdown"><?php echo($_SESSION['login']);?> <b class="caret"></b></a>
+	                        <ul class="dropdown-menu animated fadeInUp">
+	                          <li><a href="profile.html">Profile</a></li>
+	                          <li><a href="../vendor/class-cursos/functions/logout.php">Logout</a></li>
+	                        </ul>
+	                      </li>
+	                    </ul>
+	                  </nav>
+	              </div>
+	           </div>
+	        </div>
+	     </div>
+	</div>
+
+    <div class="page-content">
+    	<div class="row">
+		  <div class="col-md-2">
+		  	<div class="sidebar content-box" style="display: block;">
+                <ul class="nav">
+                    <!-- Main menu -->
+                    <li class="current"><a href="index.html"><i class="glyphicon glyphicon-home"></i> Dashboard</a></li>
+                    <li><a href="calendar.html"><i class="glyphicon glyphicon-calendar"></i> Calendar</a></li>
+                    <li><a href="stats.html"><i class="glyphicon glyphicon-stats"></i> Statistics (Charts)</a></li>                                       
+                    <li><a href="editors.html"><i class="glyphicon glyphicon-pencil"></i> Editors</a></li>
+                    <li><a href="forms.php"><i class="glyphicon glyphicon-tasks"></i> Palestras</a></li>
+                    <li class="submenu">
+                         <a href="#">
+                            <i class="glyphicon glyphicon-list"></i> Pages
+                            <span class="caret pull-right"></span>
+                         </a>
+                         <!-- Sub menu -->
+                         <ul>
+                            <li><a href="login.html">Login</a></li>
+                            <li><a href="signup.html">Signup</a></li>
+                        </ul>
+                    </li>
+                </ul>
+             </div>
+		  </div>
+
+		  <div class="col-md-10">
+		  	<div class="row">
+		  		<div class="col-md-12">
+		  			<div class="content-box-large">
+		  				<div class="panel-heading">
+							<div class="panel-title">Alunos Cadastrados</div>
+							
+							<div class="panel-options">
+								<a href="#" data-rel="collapse"><i class="glyphicon glyphicon-refresh"></i></a>
+								<a href="#" data-rel="reload"><i class="glyphicon glyphicon-cog"></i></a>
+							</div>
+						</div>
+		  				<div class="panel-body">
+							
+						
+		  	<div class="col-md-12">
+
+<div class="row">
+	<div class="col-md-12">
+		<div class="content-box-large">
+			<div class="panel-heading">
+			  <div class="panel-title">Tabelas de Alunos</div>
+			  
+			  <div class="panel-options">
+								<a href="#" data-rel="collapse"><i class="glyphicon glyphicon-refresh"></i></a>
+								<a href="#" data-rel="reload"><i class="glyphicon glyphicon-cog"></i></a>
+							</div>
+						</div>
+							<div class="panel-body">
+								<table class="table">
+								<thead>
+								<tr>
+									<th>#</th>
+									<th>Nome</th>
+									<th>Email</th>
+									<th>Palestra</th>
+									<th>Data Inscrição</th>
+
+								</tr>
+								</thead>
+								<tbody>
+								<?php
+
+									include('../vendor/class-cursos/DB/Sql.php');
+
+									$sql = new Sql();
+
+									$values = $sql->select("SELECT tb_aluno.id_aluno,tb_aluno.name_aluno,tb_aluno.email_aluno,tb_palestra.name_palestra,tb_aluno.dt_cadastro FROM tb_aluno INNER JOIN tb_palestra ON tb_aluno.id_palestra=tb_palestra.id_palestra");
+
+									foreach ($values as $key => $value):?>
+									<tr>								
+										<td><?php echo($value['id_aluno']);?></td>
+										<td><?php echo($value['name_aluno']);?></td>
+										<td><?php echo($value['email_aluno']);?></td>
+										<td><?php echo($value['name_palestra']);?></td>
+										<td><?php echo($value['dt_cadastro']);?></td>
+									</tr>
+									<?php endforeach;?>
+							
+								</tbody>
+							</table>
+							</div>
+						</div>
+					</div>
+
+
+		  				</div>
+		  			</div>
+		  		</div>
+							</div>
+		  				</div>
+		  			</div>
+		  		</div>
+		  	</div>
+
+		  	<div class="row">
+		  		<div class="col-md-12 panel-warning">
+		  			<div class="content-box-header panel-heading">
+	  					<div class="panel-title ">New vs Returning Visitors</div>
+						
+						<div class="panel-options">
+							<a href="#" data-rel="collapse"><i class="glyphicon glyphicon-refresh"></i></a>
+							<a href="#" data-rel="reload"><i class="glyphicon glyphicon-cog"></i></a>
+						</div>
+		  			</div>
+		  			<div class="content-box-large box-with-header">
+			  			Pellentesque luctus quam quis consequat vulputate. Sed sit amet diam ipsum. Praesent in pellentesque diam, sit amet dignissim erat. Aliquam erat volutpat. Aenean laoreet metus leo, laoreet feugiat enim suscipit quis. Praesent mauris mauris, ornare vitae tincidunt sed, hendrerit eget augue. Nam nec vestibulum nisi, eu dignissim nulla.
+						<br /><br />
+					</div>
+		  		</div>
+		  	</div>
+	  	</div>
+		  </div>
+		</div>
+    </div>
+
+    <footer>
+         <div class="container">
+         
+            <div class="copy text-center">
+               Copyright 2014 <a href='#'>Website</a>
+            </div>
+            
+         </div>
+      </footer>
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://code.jquery.com/jquery.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="bootstrap/js/bootstrap.min.js"></script>
+    <script src="js/custom.js"></script>
+  </body>
+</html>
